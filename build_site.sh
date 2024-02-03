14af242d8026333d1b713335392d73ec44c9f639
Jersey <wgyt735yt@gmail.com>
2024-02-03 18:09:05 -0500

    move docs to site
    Signed-off-by: Jersey <wgyt735yt@gmail.com>

--- build_site.sh
+++ build_site.sh
@@ -4,40 +4,16 @@ echo "creating build directory"
 
 rm -rf build && mkdir build
 
-cd build
-
-BUILD_DIR=$(pwd)
-
-echo "cloning bolt repo"
-
-git clone --no-checkout -q https://github.com/williamhorning/bolt >/dev/null
-
-cd bolt
-
-git sparse-checkout init --cone
-
-git sparse-checkout set packages/bolt-dash
-
-git checkout -q main
-
-echo "building bolt-dash/docs"
-
-cd packages/bolt-dash/docs
+echo "building bolt-docs"
 
 export PIP_DISABLE_PIP_VERSION_CHECK=1
 
-pip install -r requirements.txt >/dev/null
+pip install -r bolt-docs/requirements.txt >/dev/null
 
-python -m mkdocs -q build
+python -m mkdocs build -f bolt-docs/mkdocs.yml
 
-echo "finished building bolt-dash/docs"
+echo "building site"
 
-cd $BUILD_DIR
+cp -r site/* build/output
 
-echo "copying files to output direcotry"
-
-cp -r ../site output
-
-cp -r ./bolt/packages/bolt-dash/docs/output ./output/bolt/docs
-
-echo "built site! check build/output"
+echo "built! check build/output"
